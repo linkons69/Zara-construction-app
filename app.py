8f20ff1a9848bac92c70683c4fccde68013c0d0c
linkons69 <linkons69@gmail.com>
2026-02-22 15:14:01 +0600

    Refactor app.py to include GSheetsConnection

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from st_gsheets_connection import GSheetsConnection
 import pandas as pd
 from datetime import datetime
 
@@ -8,6 +9,8 @@ st.title("🏗️ কনস্ট্রাকশন সাপ্লাই চে
 
 # ২. গুগল শীট কানেকশন
 # নোট: লিঙ্কটির শেষে কোনো স্পেস রাখবেন না
+SHEET_URL = "https://docs.google.com/spreadsheets/d/1ilP26HZxJ6PviYS_dvR9S4qyr62mIttO_32QUO6O2Ro/edit?usp=sharing"
+conn = st.connection("gsheets", type=GSheetsConnection)
 
 # ডাটা লোড করা
 try:
@@ -119,5 +122,3 @@ else:
                 df.at[idx, 'Status'] = "✅ Job Done"
                 conn.update(spreadsheet=SHEET_URL, data=df)
                 st.rerun()
-
-
